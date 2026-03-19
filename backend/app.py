e868db070c481fb98766a14f5327c7ae02fc78e1
Shourya3113 <shourya3113.solanki@gmail.com>
2026-03-19 09:07:50 +0530

    fix app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,2 @@
 from app import create_app
-
 app = create_app()
-```
-
-Make sure this file is saved, then push:
-```
-git add backend/app.py
-git commit -m "fix flask entrypoint for vercel"
-git push
